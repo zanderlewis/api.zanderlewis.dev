1f3bb6cb3c137c2b1a105fba302b5e9c4f21bf4f
Zander Lewis <wolfthedev@gmail.com>
2024-07-04 15:18:44 -0400

    Update ramsay_insult.php

--- ramsay_insult.php
+++ ramsay_insult.php
@@ -6,6 +6,6 @@ $w1 = ["sandwich", "lamb chop", "salad", "soup", "steak", "pasta", "pizza", "bur
 $w2 = ["RAW", "like a shoe", "like a boot", "like a tire", "like a rock", "like a brick", "like a stone", "like a boulder", "offensive", "in need of Jesus", "like wet cardboard", "like Gandhi's flip flop"];
 $w3 = ["walnut", "absolute donkey", "dirty pig", "minger", "muppet", "plonker", "idiot sandwich", "buffoon", "clown", "doughnut", "dunce", "fool", "halfwit", "nincompoop", "numpty", "twit", "wally"];
 
-$insult = "This " . $w1[array_rand($w1)] . " is " . $w2[array_rand($w2)] . ", you " . $w3[array_rand($w3)];
+$insult = "This " . $w1[array_rand($w1)] . " is " . $w2[array_rand($w2)] . ", you " . $w3[array_rand($w3)] . "!";
 
 echo json_encode(array("insult" => $insult));
